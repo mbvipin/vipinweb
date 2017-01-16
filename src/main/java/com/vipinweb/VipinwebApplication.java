f538cd289ac23054a24ab5c5d865d187cdae3be4
Vipin Bhaskaran <mbvips@Gmail.com>
2017-01-16 13:55:21 -0500

    Initial commit

---
+++ src/main/java/com/vipinweb/VipinwebApplication.java
@@ -0,0 +1,12 @@
+package com.vipinweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VipinwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VipinwebApplication.class, args);
+	}
+}
---
+++ src/test/java/com/vipinweb/VipinwebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vipinweb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VipinwebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
